190f63e33143cf3a7c5a9def2e2812c741ccadb3
evil-snail <72147770+evil-snail@users.noreply.github.com>
2020-12-08 19:20:16 +0300

    Update first_word.py

--- TestFiles/first_word.py
+++ TestFiles/first_word.py
@@ -1,11 +1,11 @@
 lines = []
 elements = []
-with open ("def.txt", encoding="utf-8") as f:
+with open ("def.txt", encoding = "utf-8") as f:
     for line in f:
         elements = line.split()
         lines.append(elements[0].strip(","))
         elements = []
-with open ("new_def.txt", "w", encoding='utf-8') as f:
+with open ("new_def.txt", "w", encoding = "utf-8") as f:
     for line in lines:
         f.write(line)
         f.write("\n")
